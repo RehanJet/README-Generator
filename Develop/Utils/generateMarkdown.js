0d31f9655a6c76b2a840ef9494bc715603634784
RehanJet <rjetpuri5@gmail.com>
2023-05-05 22:11:14 -0500

    changes

--- Develop/Utils/generateMarkdown.js
+++ Develop/Utils/generateMarkdown.js
@@ -1,19 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'Apache 2.0 License':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GNU':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Mit':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Mozilla':
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+    case 'Python':
+      return '[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)';
+    default:
+      return ''
+  }
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  switch (license) {
+    case 'Apache 2.0 License':
+      return '[Apache 2.0 site](https://opensource.org/license/apache-2-0/)';
+    case 'GNU':
+      return '[GNU site](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Mit':
+      return '[MIT site](https://opensource.org/license/mit/)';
+    case 'Mozilla':
+      return '[Mozilla site](https://opensource.org/license/mpl-2-0/)';
+    case 'Python':
+      return '[Python site](https://www.python.org/)';
+    default:
+      return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = '';
+  if (license === 'None') {
+    licenseSection = '';
+  } else {
+    licenseSection = `License: ${license}`
+  }
+
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(answers) {
+  return `
+  
+  # Professional README Generator
+  ## ${renderLicenseSection(answers.projectLicense)} ${renderLicenseBadge(answers.projectLicense)}
+  ### ${renderLicenseLink(answers.projectLicense)}
+  ## Table of Contents
+  ### - [Project](#Project)
+  ### - [Description](#Description)
+  ### - [Installation](#Installation)
+  ### - [Project Usage](#ProjectUsage)
+  ### - [Project Contributions](#ProjectContributions)
+  ### - [Testing Instructions](#TestingInstructions)
+  ### - [License Info](#License)
+  ## Project
+  ### ${answers.Title}
+  ## Description
+  ### ${answers.Description}
+  ## Installtion
+  ### ${answers.Installation}
+  ## Usage
+  ### ${answers.Usage}
+  ## Contributors
+  ### ${answers.Contributors}
+  ## Testing Instructions
+  ### ${answers.TestInformation}
+  ## License Information
+  ### ${answers.License}
 `;
 }
 
--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,79 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./Utils/generateMarkdown');
+const generateMarkdown = require('./Utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "Title",
+    },
+
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "Description",
+    },
+
+    {
+        type: "input",
+        message: "What are the project installation steps?",
+        name: "Installation",
+    },
+
+    {
+        type: "input",
+        message: "What is the usage information for this project?",
+        name: "Usage",
+    },
+
+    {
+        type: "input",
+        message: "Who are the contributing authors of this project?",
+        name: "Contributors",
+    },
+
+    {
+        type: "input",
+        message: "What information are you testing for this project?",
+        name: "TestInformation",
+    },
+
+    {
+        type: "list",
+        name: "License",
+        message: "What license are you using for this project?",
+        choices: ['Apache 2.0 License', 'GNU', 'Mit', 'Mozilla', 'Phython'],
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const createReadMe = createFile => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./sampleReadme.md', createFile, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true
+            });
+        });
+    });
+}   
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(answers) {
+
+        var createFile = generateMarkdown(answers);
+        console.log(typeof createFile);
+        createReadeMe(createFile);
+    })
+}  
 
 // Function call to initialize app
 init();
